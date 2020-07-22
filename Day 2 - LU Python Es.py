6ad684059c31385257dd2b222d591d7a519d18b6
varunvjha <60656060+varunvjha@users.noreply.github.com>
2020-07-22 19:24:39 +0530

    Add files via upload

---
+++ Day 2 - LU Python Es.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print ("""Hello Everyone""")
+
+
+# In[4]:
+
+
+print("Hello\tPython")
+
+
+# In[5]:
+
+
+print("Hello\nPython")
+
+
+# In[6]:
+
+
+print('Python\'s world')
+
+
+# In[16]:
+
+
+name = "Varun"
+marks = 96.2
+age = 20
+print("The name of person is %s \nmarks is %.2f \nage is %d"%(name,marks,age))
+
+
+# In[17]:
+
+
+print(f"The name of person is {name} \nmarks is {marks} \nage is {age}")
+
+
+# In[18]:
+
+
+x = 20
+y=20
+
+
+# In[19]:
+
+
+id(x)
+
+
+# In[20]:
+
+
+id(y)
+
+
+# In[21]:
+
+
+del x
+
+
+# In[22]:
+
+
+del y
+
+
+# In[23]:
+
+
+z = 20
+id(z)
+
+
+# In[1]:
+
+
+a = 10
+b = 20
+c = 10
+a==b
+a==c
+
+
+# In[2]:
+
+
+a==b
+
+
+# In[3]:
+
+
+a==c
+
+
+# In[4]:
+
+
+c < a
+
+
+# In[5]:
+
+
+c <=a
+
+
+# In[6]:
+
+
+a=240
+b=1
+a | b
+
+
+# In[7]:
+
+
+a&b
+
+
+# In[1]:
+
+
+100//7
+
+
+# In[2]:
+
+
+100/7
+
+
+# In[ ]:
+
+
+
+
